e7dc2a8da8eaea6be89179515c19282ca4374d8b
MarcoZ23 <zollomarco23@gmail.com>
2026-05-05 21:46:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+function App() {
+
+  const initialData = {
+    author: '',
+    title: '',
+    body: '',
+    public: ''
+  }
+
+  const [formData, setFormData] = useState(initialData)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
