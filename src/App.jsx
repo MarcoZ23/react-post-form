e33ca6015784ebe1f81947db6e59e7107ddcf58d
MarcoZ23 <zollomarco23@gmail.com>
2026-05-05 22:33:45 +0200

    add form and function

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,66 @@ function App() {
 
   const [formData, setFormData] = useState(initialData)
 
+  function handleFormData(e) {
+
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <>
-
+      <div className="div container mt-4">
+        <div className="div row">
+          <div className="div col">
+            <form>
+              <div className="row g-3 row-cols-2">
+                <div className="col">
+                  <input
+                    className="form-control"
+                    placeholder="Autore"
+                    type="text"
+                    name="author"
+                    value={formData.author}
+                    onChange={handleFormData}
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    placeholder="Titolo"
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleFormData}
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    placeholder="Testo"
+                    type="text"
+                    name="body"
+                    value={formData.body}
+                    onChange={handleFormData}
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    className="form-control"
+                    placeholder="True o False"
+                    type="text"
+                    name="public"
+                    value={formData.public}
+                    onChange={handleFormData}
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
